ac9c03244c729d01838f520c1bcc71f051df4a81
J.Manuel <j.manuelmohedano@live.com.mx>
2024-07-01 23:27:50 -0600

    Primer comit

---
+++ src/main/java/com/aluracursos/screenmatch/principal/EjemploStreams.java
@@ -0,0 +1,11 @@
+package com.aluracursos.screenmatch.principal;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class EjemploStreams {
+    public void muestraEjemplo(){
+        List<String> nombres = Arrays.asList("Brenda","Luisa","Maria", "Fernanrda","Eric","Genesys");
+        nombres.stream().sorted().filter(n-> n.startsWith("B")).map(n-> n.toUpperCase()).limit(4).forEach(System.out::println);
+    }
+}
